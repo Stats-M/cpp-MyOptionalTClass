6502492d2b64ad86d2b59ba0a02098122767729b
Stanislav Moskalev <deafstats@live.ru>
2022-05-23 18:15:32 +0300

    Intermediate changes

--- MyOptionalTClass/optional.h
+++ MyOptionalTClass/optional.h
@@ -30,23 +30,64 @@ public:
 
     ~Optional();
 
-    bool HasValue() const;
+    bool HasValue() const
+    {
+        return is_initialized_;
+    }
 
     // Операторы * и -> не должны делать никаких проверок на пустоту Optional.
     // Эти проверки остаются на совести программиста
-    T& operator*();
-    const T& operator*() const;
-    T* operator->();
-    const T* operator->() const;
+    T& operator*()
+    {
+        return *this->value_;
+    }
+
+    const T& operator*() const
+    {
+        return *this->value_;
+    }
+
+    T* operator->()
+    {
+        return this->value_;
+    }
+
+    const T* operator->() const
+    {
+        return this->value_;
+    }
 
     // Метод Value() генерирует исключение BadOptionalAccess, если Optional пуст
-    T& Value();
-    const T& Value() const;
+    T& Value()
+    {
+        if (!this->HasValue())
+        {
+            throw BadOptionalAccess();
+        }
 
-    void Reset();
+        return *value_;
+    }
+
+    const T& Value() const
+    {
+        return *value_;
+    }
+
+    void Reset()
+    {
+        // Сначала вызываем деструктор размещенного в выделенной
+        // памяти объекта
+        value_->~T();
+        // Затем удаляем буфер
+        operator delete (data_);
+    }
 
 private:
+    // Буфер для хранения 1 объекта типа T
     // alignas нужен для правильного выравнивания блока памяти
     alignas(T) char data_[sizeof(T)];
+    // Указатель на размещенный при помощи operator new в буфере объект типа Т
+    T* value_ = nullptr;
+
     bool is_initialized_ = false;
 };
